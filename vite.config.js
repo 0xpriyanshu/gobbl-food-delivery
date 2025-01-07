93b6e50a72f6314dffc8d91ba7c60930dd886524
Priyanshu Kumar <98304940+0xpriyanshu@users.noreply.github.com>
2025-01-07 19:49:48 +0530

    vite changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gobbl-food-delivery/', // Replace with your repository name
+  base: '/gobbl-food-delivery/', // Set this to your repository name
 })
